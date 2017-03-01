738b8a923196bdd486c9e8efbe7df8771e51d2a2
birdphanom <birdoffice@yahoo.com>
2017-03-01 16:12:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,12 +38,13 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "yahooWeatherForecast":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
+    #baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    #yql_query = makeYqlQuery(req)
+   # if yql_query is None:
+   #     return {}
+   # yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    
+    result = urlopen("https://sandbox.kasikornbank.com/webhook/balance").read()
     data = json.loads(result)
     res = makeWebhookResult(data)
     return res

fd93a6fa395dbe26d649c8073c41e1c2e36956fc
birdphanom <birdoffice@yahoo.com>
2017-03-02 14:20:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def run_post():
     
     
     d = json.loads(r.text)
-    speech = d[0]["CRN_BAL_PTN_CTD"]
+    speech = "Your KBank credit card balance is " + d[0]["CRN_BAL_PTN_CTD"]
     
      
     print("Response:")

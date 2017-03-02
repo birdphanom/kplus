5e91dfa846921247ce0b9f02a396ab441adfb4af
birdphanom <birdoffice@yahoo.com>
2017-03-02 11:51:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ def run_post():
     
     
     d = json.loads(r)
-    speech = d.get('CRN_BAL_PTN_CTD')
-    #speech = r.text
+    #speech = d.get('CRN_BAL_PTN_CTD')
+    speech = r.text
     print("Response:")
     print(speech)
 
